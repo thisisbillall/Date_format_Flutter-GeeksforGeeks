2d5fc5f2baa833f264b1b6dc6a3e5ea8a23191ae
Mohammad Bilal <mdbilal7620@gmail.com>
2021-09-03 15:54:59 +0530

    intial

---
+++ android/app/src/main/kotlin/com/example/datedemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.datedemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(dateDemo());
+}
+
+class dateDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Date-Demo",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Date Format Demo"),
+          ),
+          body: Container(
+            margin: EdgeInsets.fromLTRB(120, 80, 0, 0),
+            child: Text(
+              DateFormat.yMMMEd()
+                  .format(DateTime.now()), //displaying formatted date
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )),
+    );
+  }
+}
